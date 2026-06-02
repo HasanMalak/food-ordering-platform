b60671839ab7675aed72b2a2f6c3f20cb063bf94
HasanMalak <hasanmalak215@gmail.com>
2026-06-02 12:23:06 +0530

    feat: implement user registration api

---
+++ backend/apps/accounts/serializers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "phone",
+            "password",
+        ]
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+
+        user = User(**validated_data)
+        user.role = "USER"
+        user.set_password(password)
+        user.save()
+
+        return user
